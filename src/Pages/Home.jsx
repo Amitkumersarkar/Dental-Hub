e4551d8a0ba3a55ab1fac0826088dfe3cb71fed6
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-07-29 00:05:26 +0600

    updated file

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,19 @@
-import { useLoaderData } from "react-router-dom";
+// import { useLoaderData } from "react-router-dom";
 import Banner from "../Components/Banner";
-import ServiceCard from "./ServiceCard";
+// import ServiceCard from "./ServiceCard";
 
 const Home = () => {
-    const services = useLoaderData();
+    // const services = useLoaderData();
     // console.log(services);
-    const { servicesData } = services;
+    // const { servicesData } = services;
     return (
         <div>
             <Banner></Banner>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-5">
+            {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-5">
                 {
                     servicesData.map(service => <ServiceCard key={service.id} service={service}></ServiceCard>)
                 }
-            </div>
+            </div> */}
         </div>
     );
 };
