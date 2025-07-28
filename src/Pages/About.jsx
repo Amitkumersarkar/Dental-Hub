42657c4666f3f204bb198bb865133bbe3ba819e6
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-07-28 23:43:18 +0600

    added new file

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
     console.log(feedbackData);
     return (
         <>
-            <h1>Customer FeedBack</h1>
+            <h1 className="text-2xl font-semibold">Customer FeedBack : </h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-30 m-5">
                 {
                     feedbackData.map(feedbackDa => <Feedback key={feedbackDa.id} feedbackDa={feedbackDa}></Feedback>)
